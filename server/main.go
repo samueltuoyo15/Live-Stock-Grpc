703e900b36bc1b4f5f9048daa1cf3d1b1ca17018
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-05-09 21:47:17 +0100

    added slog to logger.go

--- server/main.go
+++ server/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
- "log"
+ "log/slog"
  "net"
  "github.com/samueltuoyo15/Live-Stock-Grpc/proto/generated/stockpb"
  "github.com/samueltuoyo15/Live-Stock-Grpc/server/servers"
+ "github.com/samueltuoyo15/Live-Stock-Grpc/utils"
  "google.golang.org/grpc"
 )
 
 func main(){
+  utils.InitLogger(true)
   listen, err := net.Listen("tcp", ":8080")
   if err != nil {
-    log.Fatalf("Failed to listen: %v", err)
+    slog.Fatalf("Failed to listen: %v", err)
   }
   
-  // to create a grpc server
   grpcServer := grpc.NewServer()
   stockpb.RegisterStockServiceServer(grpcServer, &servers.StockServiceServer{})
-  log.Println("Server is listening on port :8080")
+  slog.Println("Server is listening on port :8080")
   if err := grpcServer.Serve(listen); err != nil {
-    log.Fatalf("Failed to serve: %v", err)
+    slog.Fatalf("Failed to serve: %v", err)
   }
 }
 
